7361832086a331b4e471953248bc612d17c915f3
grace <grace@han.com>
2020-01-16 22:40:01 +1100

    Variable name changes and cleaned up functions for readability

--- kohonen.py
+++ kohonen.py
@@ -4,27 +4,31 @@ import math
 import logging
 import multiprocessing
 
+
+
+
 class Kohonen:
 
-    def __init__(self):
-        # parse from config file -- for production deployment 
-        # OR from command line -- for running experiments while developing
-        self.HEIGHT = 100
-        self.WIDTH = 100
-        self.MAX_ITERATIONS 
+    def __init__(self, height, width, max_iterations, initial_learning_rate, workers, network=None, data=None, seed=42):
+        self.HEIGHT = height
+        self.WIDTH = width
+        self.MAX_ITERATIONS = max_iterations
+        self.INITIAL_LEARNING_RATE = initial_learning_rate
+        self.WORKERS = workers
+
+        self.network = network if network else self.init_network(seed)
+        self.data = data if data else self.gen_data(seed)
+
         self.INITIAL_RADIUS = max(self.WIDTH, self.HEIGHT) / 2
         self.TIME_CONST = MAX_ITERATIONS / math.log10(self.INITIAL_RADIUS)
-        self.INITIAL_LEARNING_RATE = 0.1
-        self.WORKERS = 10
-        self.network = None
-        self.data = None
 
     def init_network(self, seed):
+        """ Use a random seed for reproduceable experiments! """
         np.random.seed(seed)
         shape = (self.HEIGHT, self.WIDTH, 3)
         return np.random.random_sample(shape)
 
-    def gen_data(self, size, seed=None):
+    def gen_data(self, size, seed):
         np.random.seed(seed)
         return np.random.random((1, size, 3))
 
@@ -33,129 +37,98 @@ class Kohonen:
         d_2 = np.square(d)
         return math.sqrt(np.sum(d_2))
 
-    def distance(self, w_x, w_y, z_x, z_y):
-        x_2 = (w_x - z_x) ** 2
-        y_2 = (w_y - z_y) ** 2
+    def distance(self, node_x, node_y, bmu_x, bmu_y):
+        x_2 = (node_x - bmu_x) ** 2
+        y_2 = (node_y - bmu_y) ** 2
         d_2 = x_2 + y_2
         return math.sqrt(d_2)
     
     def learing_rate(self, timestep):
-        return INITIAL_LEARNING_RATE * math.exp(-1 * timestep / TIME_CONST)
+        return self.INITIAL_LEARNING_RATE * math.exp(-1 * timestep / self.TIME_CONST)
     
     def radius(self, timestep):
-        return INITIAL_RADIUS * math.exp(-1 * timestep / TIME_CONST)
+        return self.INITIAL_RADIUS * math.exp(-1 * timestep / self.TIME_CONST)
     
-    # return if node is located within the radius
-    # around node (centre)
-    def in_radius(self, x_c, y_c, x_n, y_n, r):
-        return (x_n - x_c) ** 2 + (y_n - y_c) ** 2 < r ** 2
+    def in_radius(self, centre_x, centre_y, node_x, node_y, r):
+        """ return if node is located within the radius
+            around `centre_x, centre_y` """
+        return (node_x - centre_x) ** 2 + (node_y - centre_y) ** 2 < r ** 2
     
-    # influence decay
-    def influence(self, w_x, w_y, z_x, z_y, t):
-        d = distance(w_x, w_y, z_x, z_y)
-        r = radius(t)
+    def calculate_influence(self, node_x, node_y, bmu_x, bmu_y, timestep):
+        d = self.distance(node_x, node_y, bmu_x, bmu_y)
+        r = self.radius(t)
         return math.exp((-1 * d ** 2) / (2 * r ** 2))
     
-    # update weights
-    def update(self, network, w_x, w_y, v_x, v_y, t):
-        w = network[w_x][w_y]
-        v = network[v_x][v_y]
-        d = np.subtract(v, w)
-        inf = influence(w_x, w_y, v_x, v_y, t)
-        lr = learing_rate(t)
-        scalar = lr * inf
-        correction = np.multiply(scalar, d)
-        return np.add(w, correction)
+    def update_weights(self, node_x, node_y, bmu_x, bmu_y, timestep):
+        node = self.network[node_x][node_y]
+        bmu = self.network[bmu_x][bmu_y]
+        d = np.subtract(bmu, node)
 
-    def bmu(self, in_vec, network):
-        min = float("inf")
-        b = None
-        for i in range(len(network)):
-            for j in range(len(network[i])):
-                dist = distance_weight(in_vec, network[i][j])
+        # calculate influence and learning rate
+        influence = self.calculate_influence(node_x, node_y, bmu_x, bmu_y, timestep)
+        learning_rate = self.calculate_learing_rate(timestep)
+        scalar = influence * learning_rate
+        correction = np.multiply(scalar, d)
+        return np.add(node, correction)
+
+    def get_bmu(self, in_vec):
+        bmu = None
+        for col in range(len(self.network)):
+            for row in range(len(self.network[i])):
+                # use multiprocessing to parallelize euclidean distance calculation
+                # pool = multiprocessing.Pool(self.WORKERS)
+                dist = self.distance_weight(in_vec, self.network[col][row])
                 if dist < min:
                     min = dist
-                    b = (network[i][j], i, j)
-                # print(f"euclidean dist: {distance_weight(in_vec, network[i][j])}")
-        # print(bmu)
-        return b
+                    bmu = (col, row)
+        return bmu
     
-    def train(self, network, data):
-        for t in range(1, self.MAX_ITERATIONS):
-            ind = t % len(data[0])
-            in_vec = data[0][ind]
-            b = bmu(in_vec, network)
-            print(f" invec {in_vec}, bmu {b}")
+    def train(self):
+        """ Updates `network` for """
+        for timestep in range(1, self.MAX_ITERATIONS):
+            i = t % len(data[0])
+            in_v = data[0][i]
+            bmu = self.get_bmu(in_v, self.network)
             
-            r = radius(t)
-            for i in range(len(network)):
-                for j in range(len(network[i])):
-                    if in_radius(b[1],b[2], i, j, r):
-                        prev = network[i][j]
-                        up = update(network, i, j, b[1], b[2], t)
-                        print(f"prev {prev}, up {up}")
-                        network[i][j] = up
+            r = self.radius(timestep)
+            for col in range(len(self.network)):
+                for row in range(len(self.network[i])):
+                    if self.in_radius(bmu[0], bmu[1], col, row, r):
+                        self.network[col][row] = self.update_weights(col, row, bmu[0], bmu[1], timestep)
+
+    def show(self):
+        """ Graphical output of Kohonen network """
+        plt.imshow(self.network)
+        plt.show()
+        return
 
 
+if __name__ == "__main__":
+    # Parse from config file -- for CI/CD and production deployment
+    # Or from command line -- for running experiments while developing
+    HEIGHT = 100
+    WIDTH = 100
+    MAX_ITERATIONS = 1000
+    INITIAL_LEARNING_RATE = 0.1
+    WORKERS = 10
 
-r = 5
-for i in range(len(network)):
-    for j in range(len(network[i])):
-        if in_radius(5, 5, i, j, r):
-            network[i][j] = np.array([0, 0, 0])
-
-plt.imshow(network)
-plt.show()
-
-            
-
-print(data)
-plt.imshow(data)
-# plt.imshow(network)
-plt.show()
-#%%
-
-
-# bmu = bmu(data[0], network)
-# print(bmu)
-# r = radius(1)
-# in_r = []
-# for i in range(len(network)):
-#     for j in range(len(network[i])):
-#         if in_radius(2, 5, i, j, r):
-#             in_r.append((i, j))
-            
-#             print(f"UPDATED {update(bmu[0], network[i][j], bmu[1], bmu[2], i, j, 1)}")
-            # network[i][j] = np.array([0, 0, 0])
-#%%
-
-# print(in_r)
-# print(network)
-train(network, data)
-# print(network)
-# print(data)
-print(data)
-
-plt.imshow(data)
-plt.imshow(network)
-plt.show()
-#%%
-
-#%%
-rgb = lambda x: np.array([x[0]/255, x[1]/255, x[2]/255])
-
-
-c = [np.array([255,102,102]), np.array([255,178,102]), np.array([178,255,102]), np.array([102,255,102]), np.array([102,255,178]), np.array([102,255,255
-]), np.array([102,178,255]), np.array([102,102,255]), np.array([178,102,255]), np.array([255,102,255]), np.array([255, 102, 178])]
-
-x = []
-for i in c:
-    y = rgb(i)
-    x.append(y)
-data = np.array([x])
+    # We should be able to import a partially trained network and training data on disk
+    DATA = None
+    network = None
+    k = Kohonen()
 
 
 
 
 
-# %%
+# rgb = lambda x: np.array([x[0]/255, x[1]/255, x[2]/255])
+
+
+# c = [np.array([255,102,102]), np.array([255,178,102]), np.array([178,255,102]), np.array([102,255,102]), np.array([102,255,178]), np.array([102,255,255
+# ]), np.array([102,178,255]), np.array([102,102,255]), np.array([178,102,255]), np.array([255,102,255]), np.array([255, 102, 178])]
+
+# x = []
+# for i in c:
+#     y = rgb(i)
+#     x.append(y)
+# data = np.array([x])
